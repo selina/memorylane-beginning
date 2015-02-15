6f0cf380fb0a96d7d137a5a2772d31d53fb3063d
Selina <enigmacat@users.noreply.github.com>
2015-02-15 06:44:27 -0500

    flask crap etc

--- app.py
+++ app.py
@@ -1,7 +1,31 @@
-import requests
-import json
+# import requests
+# import json
 
-data = requests.get("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCj_bAQA7Rmxebtgaq08ykLXl1cxA03o0c").text
+# data = requests.get("https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyCj_bAQA7Rmxebtgaq08ykLXl1cxA03o0c").text
 
 
-print(data)
+# print(data)
+
+
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("index.html")
+
+@app.route("/search", methods=["GET", "POST"])
+def search():
+	if request.method == "POST":
+		longitude = request.form["longitude"]
+		latitude = request.form["latitude"]
+		print(longitude)
+		print(latitude)	
+		return render_template("index2.html", longitude=longitude, latitude=latitude)
+	else:
+		return "Error, No data transerred"
+
+
+# if __name__ == "__main__":
+#     
+app.run(debug=True)
